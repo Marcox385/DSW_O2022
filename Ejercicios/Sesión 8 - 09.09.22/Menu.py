b7b3532b24ddcd31984b2300885300c64f5635f3
MarCordero <marcko_385@hotmail.com>
2022-09-13 13:18:09 -0500

    Entregable Sesión 9
    Ejercicio - reparación de métodos

---
+++ Ejercicios/Sesión 8 - 09.09.22/Menu.py
@@ -0,0 +1,12 @@
+# IS727272 - Cordero Hernández, Marco Ricardo
+class Menu:
+    def __init__(self,  productos:'list[Producto]', categoria:str = 'Normal') -> None:
+        ''' Retorna una nueva instancia de menú '''  
+        self.categoria:str = categoria
+        self.produtos:list[Producto] = productos
+    
+    def desplegar_menu(self):
+        ''' Desplegar menú con sus productos correspondientes '''
+        print(f'Menú', f'de {self.categoria}' if (self.categoria != 'Normal') else '')
+        for p in range(len(self.productos)):
+            print(f'{p + 1} - {self.productos[p].nombre} - ${self.productos[p].precio}')
